75263155a7bdac5f1d37a6dd09053688f9db691c
jaywcjlove <398188662@qq.com>
2024-01-20 19:24:58 +0800

    update resume-revise page head description.

--- src/app/resume-revise/index.tsx
+++ src/app/resume-revise/index.tsx
@@ -12,7 +12,8 @@ export const Component: FC<PropsWithChildren> = () => {
   const { t } = useTranslation('resume-revise');
   return (
     <Header background="#31467b">
-      <Head.Title>Resume Revise {t('detail')}</Head.Title>
+      <Head.Title>Resume Revise</Head.Title>
+      <Head.Meta name="description" content={t('detail')} />
       <Head.Link rel="icon" type="image/png" href={resumeRevise} />
       <Head.Meta name="twitter:creator" content="@jaywcjlove" />
       <Head.Meta name="twitter:title" content="Resume Revise" />
