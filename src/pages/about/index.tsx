cae8600f0408bab50f6cb964754d80ca88133cf0
小弟调调 <398188662@qq.com>
2025-06-14 18:34:21 +0800

    doc: update about.

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,6 +1,36 @@
 import { FC, PropsWithChildren } from 'react';
 import { useTranslation, Trans } from 'react-i18next';
 import { Wrapper } from '../../comps/Layout';
+import { ReactComponent as PeerlistIcon } from '../../assets/peerlist.svg';
+import { ReactComponent as RedditIcon } from '../../assets/reddit.svg';
+import { ReactComponent as XIcon } from '../../assets/x.svg';
+import { ReactComponent as BlueskyIcon } from '../../assets/bluesky.svg';
+import { ReactComponent as GithubIcon } from '../../assets/github2.svg';
+import { ReactComponent as WeiboIcon } from '../../assets/weibo.svg';
+import { ReactComponent as NpmIcon } from '../../assets/npm.svg';
+import { ReactComponent as SwiftpackageIcon } from '../../assets/swiftpackageindex.svg';
+import { ReactComponent as DockerIcon } from '../../assets/docker.svg';
+import { ReactComponent as ProducthuntIcon } from '../../assets/producthunt.svg';
+import { ReactComponent as InstagramIcon } from '../../assets/instagram.svg';
+import { ReactComponent as ThreadsIcon } from '../../assets/threads.svg';
+import { ReactComponent as MastodonIcon } from '../../assets/mastodon.svg';
+import styled, { css } from 'styled-components';
+
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  svg {
+    font-size: 1.1rem;
+  }
+  a {
+    display: inline-flex;
+    align-items: center;
+    gap: 0.35em;
+    line-height: 1.5;
+  }
+`;
 
 export const Component: FC<PropsWithChildren> = () => {
   const { t } = useTranslation();
@@ -21,47 +51,86 @@ export const Component: FC<PropsWithChildren> = () => {
       <p>{t('about.info3')}</p>
       <p>{t('about.info4')}</p>
       <h2>{t('about.subtitle')}</h2>
-      <ul>
+      <List>
         <li>
-          <a href="https://twitter.com/intent/follow?original_referer=https%3A%2F%2Fwangchujiang.com&twgr=kettanaito&screen_name=jaywcjlove&twterm=follow" target="_blank">Twitter</a>
+          <a href="https://github.com/jaywcjlove" target="_blank">
+            <GithubIcon />
+            Github
+          </a>
         </li>
         <li>
-          <a href="https://www.reddit.com/user/wcjiang/submitted/" target="_blank">Reddit</a>
+          <a href="https://twitter.com/intent/follow?original_referer=https%3A%2F%2Fwangchujiang.com&twgr=kettanaito&screen_name=jaywcjlove&twterm=follow" target="_blank">
+            <XIcon />
+            Twitter
+          </a>
         </li>
         <li>
-          <a href="https://peerlist.io/jiang" target="_blank">Peerlist</a>
+          <a href="https://www.reddit.com/user/wcjiang/submitted/" target="_blank">
+            <RedditIcon />
+            Reddit
+          </a>
         </li>
         <li>
-          <a href="https://bsky.app/profile/wangchujiang.com" target="_blank">Bluesky</a>
+          <a href="https://bsky.app/profile/wangchujiang.com" target="_blank">
+            <BlueskyIcon />
+            Bluesky
+          </a>
         </li>
         <li>
-          <a href="https://github.com/jaywcjlove" target="_blank">Github</a>
+          <a href="https://peerlist.io/jiang" target="_blank">
+            <PeerlistIcon />
+            Peerlist
+          </a>
         </li>
         <li>
-          <a href="https://weibo.com/pc175" target="_blank">微博</a>
+          <a href="https://weibo.com/pc175" target="_blank">
+            <WeiboIcon />
+            微博
+          </a>
         </li>
         <li>
-          <a href="https://www.npmjs.com/~wcjiang" target="_blank">npm</a>
+          <a href="https://www.npmjs.com/~wcjiang" target="_blank">
+            <NpmIcon />
+            npm
+          </a>
         </li>
         <li>
-          <a href="https://swiftpackageindex.com/jaywcjlove" target="_blank">Swift Package Index</a>
+          <a href="https://swiftpackageindex.com/jaywcjlove" target="_blank">
+            <SwiftpackageIcon />
+            Swift Package Index
+          </a>
         </li>
         <li>
-          <a href="https://hub.docker.com/u/wcjiang" target="_blank">Docker Hub</a>
+          <a href="https://hub.docker.com/u/wcjiang" target="_blank">
+            <DockerIcon />
+            Docker Hub
+          </a>
         </li>
         <li>
-          <a href="https://www.producthunt.com/@jaywcjlove" target="_blank">Product Hunt</a>
+          <a href="https://www.producthunt.com/@jaywcjlove" target="_blank">
+            <ProducthuntIcon />
+            Product Hunt
+          </a>
         </li>
         <li>
-          <a href="https://www.instagram.com/wangchujiang/" target="_blank">Instagram</a>
+          <a href="https://www.instagram.com/wangchujiang/" target="_blank">
+            <InstagramIcon />
+            Instagram
+          </a>
         </li>
         <li>
-          <a href="https://www.threads.com/@wangchujiang" target="_blank">Threads</a>
+          <a href="https://www.threads.com/@wangchujiang" target="_blank">
+            <ThreadsIcon />
+            Threads
+          </a>
         </li>
         <li>
-          <a href="https://mastodon.social/@jaywcjlove" target="_blank">Mastodon</a>
+          <a href="https://mastodon.social/@jaywcjlove" target="_blank">
+            <MastodonIcon />
+            Mastodon
+          </a>
         </li>
-      </ul>
+      </List>
     </Wrapper>
   );
 };
