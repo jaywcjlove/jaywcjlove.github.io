96e6401ee2b269288e0de4950a0fbbae37aaa4c2
jaywcjlove <jaywcjlove@users.noreply.github.com>
2026-03-23 04:06:05 +0000

    website: update sponsor. ab3db60f6a0eb3db9913aa25f5f8dababd724977

---
+++ static/js/959.0a9f9a6a.chunk.js
@@ -0,0 +1,25 @@
+"use strict";(globalThis.webpackChunkjaywcjlove_github_io=globalThis.webpackChunkjaywcjlove_github_io||[]).push([[221,959],{407(e,t,r){r.r(t),r.d(t,{Component:()=>g});var n=r(403),i=r(4117),s=r(8793),a=r(4211),o=r(9356),c=r(9128),h=r(250),l=r(1574),p=r(1232),m=r(3027),j=r(579);const d=(0,n.Ay)(s.Wrapper)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding: 10rem 2rem 5rem 2rem;
+  h1 {
+    font-size: 1.2rem;
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  > * {
+    margin-top: 0;
+  }
+  img {
+    margin-top: 0.75em;
+    border-radius: 10%;
+  }
+  section {
+    display: flex;
+    gap: 1rem;
+  }
+`,g=()=>{const{t:e}=(0,i.Bd)(),{t:t}=(0,i.Bd)("app");return(0,j.jsxs)(j.Fragment,{children:[(0,j.jsxs)(d,{children:[(0,j.jsx)("img",{src:a,width:"60"}),(0,j.jsx)("h1",{children:"\u5c0f\u5f1f\u8c03\u8c03\u2122"}),(0,j.jsxs)("section",{children:[(0,j.jsx)("a",{href:"https://weibo.com/pc175",target:"_blank",children:(0,j.jsx)(l.h,{})}),(0,j.jsx)("a",{href:"https://twitter.com/intent/follow?original_referer=https%3A%2F%2Fwangchujiang.com&twgr=kettanaito&screen_name=jaywcjlove&twterm=follow",target:"_blank",children:(0,j.jsx)(h.h,{})}),(0,j.jsx)("a",{href:"https://github.com/jaywcjlove",target:"_blank",children:(0,j.jsx)(c.h,{})}),(0,j.jsx)("a",{href:"mailto:kennyiseeyou@gmail.com",children:(0,j.jsx)(o.h,{})})]}),(0,j.jsx)("p",{children:e("home.info")})]}),(0,j.jsx)(m.AppWrapper,{children:(0,j.jsx)(m.AppContent,{children:p.map((e,r)=>{const n={.../^(https?:\/\/)?([\w.]+)\.([a-z]{2,})(\/\S*)?$/i.test(e.path)?{target:"_blank"}:{}};return(0,j.jsxs)(m.App,{to:e.path,...n,children:[(0,j.jsx)("img",{src:e.icon}),(0,j.jsxs)("section",{children:[(0,j.jsx)("sup",{children:e.platform}),(0,j.jsx)("h3",{children:t(`${e.name}`)}),(0,j.jsx)("p",{children:t(`${e.name}_`)})]})]},r)})})})]})}}}]);
+//# sourceMappingURL=959.0a9f9a6a.chunk.js.map
